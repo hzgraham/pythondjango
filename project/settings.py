0624484f2fcfbc2c8361892759703e7fdaa78aa5
hgraham <hgraham@redhat.com>
2015-07-08 16:55:13 -0400

    remaking

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv(
     'DJANGO_SECRET_KEY',
     # safe value used for development when DJANGO_SECRET_KEY might not be set
-    'djangopw'
+    '9e4@&tw46$l31)zrqe3wi+-slqm(ruvz&se0^%9#6(_w3ui!c0'
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -43,7 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'buddy1',
+    'welcome',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -51,10 +51,10 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'project.urls'
--- project/urls.py
+++ project/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 admin.autodiscover()
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'buddy1.views.index'),
+    url(r'^$', 'welcome.views.index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
